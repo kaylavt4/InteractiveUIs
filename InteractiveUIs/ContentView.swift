8760eff84a85eac7624d1c3e428c647a6237e5e5
Kayla Tabalon <kaylatabalon@gmail.com>
2024-07-12 13:02:16 -0400

    Initial Commit

---
+++ InteractiveUIs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InteractiveUIs
+//
+//  Created by Kayla Tabalon on 7/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InteractiveUIs/InteractiveUIsApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveUIsApp.swift
+//  InteractiveUIs
+//
+//  Created by Kayla Tabalon on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveUIsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
